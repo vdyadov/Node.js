9bbd86539b3b96d1ea619dbef0803ddd182fc869
vdyadov <vdyadov@yandex.ru>
2021-12-27 00:39:43 +0700

    Глава 1 задание 5

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
- 
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(request, response) {
+    response.end("Hello from Express");   
+});
+
+app.listen(5000);
