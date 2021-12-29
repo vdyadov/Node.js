9834640c4d23af88ac4375180967f4601d8ef477
vdyadov <vdyadov@yandex.ru>
2021-12-29 11:54:56 +0700

    Глава 2 задание 11

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const fs = require("fs");
  
-let writeableStream = fs.createWriteStream("test.txt");
-writeableStream.write("Привет мир!\n");
-writeableStream.write("Продолжение записи \n");
-writeableStream.end("Завершение записи\n");
 let readableStream = fs.createReadStream("test.txt", "utf8");
  
-readableStream.on("data", function(chunk){ 
-    console.log(chunk);
-});
+let writeableStream = fs.createWriteStream("test1.txt");
+ 
+readableStream.pipe(writeableStream);
