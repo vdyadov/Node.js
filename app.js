0d208e0af8436efb859dd053eb3f5a6bfde78a9c
vdyadov <vdyadov@yandex.ru>
2021-12-29 11:38:27 +0700

    Глава 2 задание 8

--- app.js
+++ app.js
@@ -9,3 +9,28 @@ fs.readFile("test.txt", "utf8",
 
 console.log("Cинхронное чтение");
 console.log(fs.readFileSync("test.txt", "utf8"));
+
+fs.writeFile("test1.txt", "Асинхронная запись\n", 
+    function (error, data) {
+        console.log("Асинхронная запись в файл. Содержимое файла");
+        if (error) throw err;
+        data = fs.readFileSync("test1.txt", "utf8");
+        console.log(data);
+});
+
+console.log("Синхронная запись в файл. Содержимое файла");
+fs.writeFileSync("test1.txt", "Синхронная запись\n");
+console.log(fs.readFileSync("test1.txt", "utf8"));
+
+fs.appendFile("test.txt", "Асинхронное дополнение\n", 
+    function (error, data) {
+        console.log("Асинхронное дополнение в файл. Содержимое файла");
+        if (error) throw err;
+        data = fs.readFileSync("test.txt", "utf8");
+        console.log(data);
+});
+
+console.log("Синхронное дополнение в файл. Содержимое файла");
+fs.appendFileSync("test.txt", "Синхронное дополнениe\n");
+console.log(fs.readFileSync("test.txt", "utf8"));
+
