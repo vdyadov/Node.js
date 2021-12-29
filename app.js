14b6c3a707272ca8b95da7f1b1fb6bb6f9128943
vdyadov <vdyadov@yandex.ru>
2021-12-29 11:46:34 +0700

    Глава 2 задание 9

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
-const fs = require("fs");
-
-fs.readFile("test.txt", "utf8", 
-    function (error, data) {
-        console.log("Асинхронное чтение");
-        if (error) throw err;
-        console.log(data);
+const EventEmitter = require("events");
+  
+let eventName = "greet";
+ 
+class User extends EventEmitter {
+    sayHi(data) {
+        this.emit(eventName, data);
+    }
+}
+ 
+let user = new User();
+// добавляем к объекту user обработку события "greet"
+user.on(eventName, function(data){
+    console.log(data);
 });
-
-console.log("Cинхронное чтение");
-console.log(fs.readFileSync("test.txt", "utf8"));
-
-fs.writeFile("test1.txt", "Асинхронная запись\n", 
-    function (error, data) {
-        console.log("Асинхронная запись в файл. Содержимое файла");
-        if (error) throw err;
-        data = fs.readFileSync("test1.txt", "utf8");
-        console.log(data);
-});
-
-console.log("Синхронная запись в файл. Содержимое файла");
-fs.writeFileSync("test1.txt", "Синхронная запись\n");
-console.log(fs.readFileSync("test1.txt", "utf8"));
-
-fs.appendFile("test.txt", "Асинхронное дополнение\n", 
-    function (error, data) {
-        console.log("Асинхронное дополнение в файл. Содержимое файла");
-        if (error) throw err;
-        data = fs.readFileSync("test.txt", "utf8");
-        console.log(data);
-});
-
-console.log("Синхронное дополнение в файл. Содержимое файла");
-fs.appendFileSync("test.txt", "Синхронное дополнениe\n");
-console.log(fs.readFileSync("test.txt", "utf8"));
-
+  
+user.sayHi("Мне нужна твоя одежда...");
