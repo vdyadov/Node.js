5bb9d55cfbefb765c61794c99c062479fc1fc5cd
vdyadov <vdyadov@yandex.ru>
2022-01-10 01:13:34 +0700

    Глава 4 Задание 2

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const fs = require("fs");
  
 const app = express();
+app.use(function(request, response, next){
+     
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get("user-agent")}`;
+    console.log(data);
+    fs.appendFile("server.log", data + "\n", function(){});
+    next();
+});
+ 
 app.get("/", function(request, response){
-     
-    response.send("<h1>Главная страница</h1>");
-});
-app.get("/about", function(request, response){
-     
-    response.send("<h1>О сайте</h1>");
-});
-app.get("/contact", function(request, response){
-     
-    response.send("<h1>Контакты</h1>");
+    response.send("Hello");
 });
 app.listen(5000, () => console.log("Server started at 5000 port"));
