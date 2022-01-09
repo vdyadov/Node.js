98495315ef0c132277a00805297fbcf69844d2c9
vdyadov <vdyadov@yandex.ru>
2022-01-09 21:06:44 +0700

    Глава 3 Задание 2.2

--- app.js
+++ app.js
@@ -9,13 +9,18 @@ http.createServer(function(request, response){
     // проверяем есть ли такой файл
     fs.access(filePath, fs.constants.R_OK, err => {
         // если произошла ошибка - отправляем статусный код 404
-        if(err){
-            response.statusCode = 404;
-            response.end("Resourse not found!");
-        }
-        else{
-            fs.createReadStream(filePath).pipe(response);
-        }
+        fs.readFile(filePath, function(err, data){
+              
+            if(err){
+                      
+                response.statusCode = 404;
+                response.end("Resourse not found!");
+            }   
+            else{
+                response.end(data);
+            }
+        });
+        return err;
       });
 }).listen(5000, function(){
     console.log("Server started at 5000 port");
