db18321fff021b26d04dabf97433b871e0bbbd4b
vdyadov <vdyadov@yandex.ru>
2022-01-09 18:16:27 +0700

    Глава 3 Задание 1.2

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ http.createServer(function(request, response){
      
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
      
-    if(request.url === "/home" || request.url === "/"){
-        response.write("<h2>Home</h2>");
+    if(request.url === "/"){
+        response.statusCode = 302; // временная переадресация
+        // на адрес localhost:3000/newpage
+        response.setHeader("Location", "/newpage");
     }
-    else if(request.url == "/about"){
-        response.write("<h2>About</h2>");
-    }
-    else if(request.url == "/contact"){
-        response.write("<h2>Contacts</h2>");
+    else if(request.url == "/newpage"){
+        response.write("New address");
     }
     else{
-        response.write("<h2>Not found</h2>");
+        response.statusCode = 404; // адрес не найден
+        response.write("Not Found");
     }
     response.end();
 }).listen(5000);
