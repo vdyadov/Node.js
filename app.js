88201f875d1881d68c0aaa0b340aaf7592d46a0a
vdyadov <vdyadov@yandex.ru>
2022-01-09 22:44:57 +0700

    Глава 3 Задание 4

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const http = require("http");
 const fs = require("fs");
   
-http.createServer(async (request, response) => {
+http.createServer((request, response) => {
       
-    if (request.url === "/user") {
-        
-        const buffers = []; // буфер для получаемых данных
-
-        for await (const chunk of request) {
-            buffers.push(chunk);        // добавляем в буфер полученные данные
-        }
-
-        const user = JSON.parse(Buffer.concat(buffers).toString());
-        console.log(user.name,"-", user.age);
-        response.end("Данные успешно получены");
-    }
-    else{
-        fs.readFile("index.html", (error, data) => response.end(data));
-    }
+    fs.readFile("index.html", "utf8", function(error, data){
+                 
+        let message = "Изучаем Node.js"; 
+        let header = "Главная страница";
+        data = data.replace("{header}", header).replace("{message}", message);
+        response.end(data);
+    })
 }).listen(5000, () => console.log("Server started at 5000 port"));
