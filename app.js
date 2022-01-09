c2d968f05383210d0a0fe537d030151ed15bc053
vdyadov <vdyadov@yandex.ru>
2022-01-09 18:16:02 +0700

    Глава 3 Задание 1.1

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const fs = require("fs");
- 
-let readableStream = fs.createReadStream("test.txt", "utf8");
- 
-let writeableStream = fs.createWriteStream("test1.txt");
- 
-readableStream.pipe(writeableStream);
+const http = require("http");
+  
+http.createServer(function(request, response){
+     
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+     
+    if(request.url === "/home" || request.url === "/"){
+        response.write("<h2>Home</h2>");
+    }
+    else if(request.url == "/about"){
+        response.write("<h2>About</h2>");
+    }
+    else if(request.url == "/contact"){
+        response.write("<h2>Contacts</h2>");
+    }
+    else{
+        response.write("<h2>Not found</h2>");
+    }
+    response.end();
+}).listen(5000);
