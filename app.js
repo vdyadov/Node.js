1c3bc1f16c86aff0885a0b173a0c950e44df9301
vdyadov <vdyadov@yandex.ru>
2022-01-09 21:26:56 +0700

    Глава 3 Задание 3.1

--- app.js
+++ app.js
@@ -3,25 +3,18 @@ const fs = require("fs");
   
 http.createServer(function(request, response){
      
-    console.log(`Запрошенный адрес: ${request.url}`);
-    // считываем путь
-    const filePath = request.url.substr(1);
-    // проверяем есть ли такой файл
-    fs.access(filePath, fs.constants.R_OK, err => {
-        // если произошла ошибка - отправляем статусный код 404
-        fs.readFile(filePath, function(err, data){
-              
-            if(err){
-                      
-                response.statusCode = 404;
-                response.end("Resourse not found!");
-            }   
-            else{
-                response.end(data);
-            }
+    if (request.url === "/user") {
+         
+        let data = "";
+        request.on("data", chunk => {
+            data += chunk;
         });
-        return err;
-      });
-}).listen(5000, function(){
-    console.log("Server started at 5000 port");
-});
+        request.on("end", () => {
+            console.log(data);
+            response.end("get data is ok!");
+        });
+    }
+    else{
+        fs.readFile("index.html", (error, data) => response.end(data));
+    }
+}).listen(5000, () => console.log("Server started at 5000 port"));
