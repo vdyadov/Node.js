8445df5437662a3a3d5d6e23284e643b6e43e17e
vdyadov <vdyadov@yandex.ru>
2022-01-09 21:00:13 +0700

    Глава 3 Задание 2.1

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const http = require("http");
+const fs = require("fs");
   
 http.createServer(function(request, response){
      
-    response.setHeader("Content-Type", "text/html; charset=utf-8;");
-     
-    if(request.url === "/"){
-        response.statusCode = 302; // временная переадресация
-        // на адрес localhost:3000/newpage
-        response.setHeader("Location", "/newpage");
-    }
-    else if(request.url == "/newpage"){
-        response.write("New address");
-    }
-    else{
-        response.statusCode = 404; // адрес не найден
-        response.write("Not Found");
-    }
-    response.end();
-}).listen(5000);
+    console.log(`Запрошенный адрес: ${request.url}`);
+    // считываем путь
+    const filePath = request.url.substr(1);
+    // проверяем есть ли такой файл
+    fs.access(filePath, fs.constants.R_OK, err => {
+        // если произошла ошибка - отправляем статусный код 404
+        if(err){
+            response.statusCode = 404;
+            response.end("Resourse not found!");
+        }
+        else{
+            fs.createReadStream(filePath).pipe(response);
+        }
+      });
+}).listen(5000, function(){
+    console.log("Server started at 5000 port");
+});
