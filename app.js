9bb836ecbf7c4a630873c29b038dd151b335fc9a
vdyadov <vdyadov@yandex.ru>
2022-01-10 15:52:09 +0700

    Express.Router

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ const express = require("express");
  
 const app = express();
 
+const prodRouter = express.Router();
+ 
+prodRouter.use("/create", function (request, response) {
+    response.send("Добавление товара");
+});
 
-const urlencodedParser = express.urlencoded({extended: false});
+prodRouter.use("/:id", function (request, response) {
+    response.send(`Товар ${request.params.id}`);
+});
 
-app.get("/categories/:catId/products/:prodId", function(request, response) {
-    let catId = request.params["catId"];
-    let prodId = request.params["prodId"];
-    response.send(`Категория: ${catId}  Товар: ${prodId}`);
+prodRouter.use("/", function (request, response) {
+    response.send("Список товаров");
+});
+
+app.use("/about", function (request, response) {
+    response.send("О сайте");
+});
+
+app.use("/products", prodRouter);
+
+app.use("/", function (request, response) {
+    response.send("Главная страница");
 });
 
 app.listen(5000, () => console.log("Server started at 5000 port"));
