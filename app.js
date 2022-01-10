da3f7ada9172b0cb8800eee74978b9a802c4e1d1
vdyadov <vdyadov@yandex.ru>
2022-01-10 15:47:21 +0700

    Express.Параметры маршрута

--- app.js
+++ app.js
@@ -5,16 +5,10 @@ const app = express();
 
 const urlencodedParser = express.urlencoded({extended: false});
 
-app.get("/", function(request, response) {
-    response.sendFile(__dirname + "/index.html");
-});
-
-app.post("/", urlencodedParser, function(request, response) {
-    if(!request.body) 
-        return response.sendStatus(400);
-    
-    console.log(request.body);
-    response.send(`${request.body.userName} - ${request.body.userAge}`);
+app.get("/categories/:catId/products/:prodId", function(request, response) {
+    let catId = request.params["catId"];
+    let prodId = request.params["prodId"];
+    response.send(`Категория: ${catId}  Товар: ${prodId}`);
 });
 
 app.listen(5000, () => console.log("Server started at 5000 port"));
